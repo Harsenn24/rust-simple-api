c1cff62996338e4e4b5d5edad2ce962d86a4519a
harsenn <harsenn@skinmystery.com>
2023-01-13 09:55:09 +0700

    dynamic struct

--- rust-api/src/data2/file_1.rs
+++ rust-api/src/data2/file_1.rs
@@ -1,3 +1,19 @@
-pub fn file1() -> String{
+use serde::{Deserialize, Serialize};
+
+pub fn file1() -> String {
     "ini dari dalam folder data 2".to_string()
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UserData<T> {
+    pub id: i32,
+    pub name: String,
+    pub age: i32,
+    pub anything: T,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Params {
+    pub number: Option<i32>,
+    pub letter: Option<String>,
 }
--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,11 +1,27 @@
+use crate::data2::file_1::{Params, UserData};
+
 mod data1;
 mod data2;
 
 fn main() {
     let result_data1 = data1::home::rumah();
-    let result_data2= data2::file_1::file1();
+    let result_data2 = data2::file_1::file1();
 
     println!("{}", result_data1);
 
-    println!("{}", result_data2)
+    println!("{}", result_data2);
+
+    let paramsku = Params {
+        number: Some(1),
+        letter: Some("namaku adalah".to_string()),
+    };
+
+    let user = UserData::<Params> {
+        id: 2,
+        name: "harsenn".to_string(),
+        age: 28,
+        anything: paramsku,
+    };
+
+    println!("{:?}", user)
 }
