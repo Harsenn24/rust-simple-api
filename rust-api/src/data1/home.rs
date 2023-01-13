3f11f9e1503254dfbd249502c672a7fcd95c0e90
harsenn <harsenn@skinmystery.com>
2023-01-13 10:32:52 +0700

    serialize and deserialize json payload

--- rust-api/src/data1/home.rs
+++ rust-api/src/data1/home.rs
@@ -1,7 +1,16 @@
 use crate::data2::data3::file_3;
+use serde::{Deserialize, Serialize};
 
 pub fn rumah() -> String {
     let file_from_folder_3: String = file_3::file3();
     let new_string: String = format!("{}", file_from_folder_3);
     new_string
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Person {
+    pub name: String,
+    pub year_born: i32,
+    pub phones: Vec<String>,
+}
+ 
--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,4 +1,5 @@
-use crate::data2::file_1::{Params, UserData};
+use crate::{data2::file_1::{Params, UserData}, data1::home::Person};
+use serde_json::{json, to_value};
 
 mod data1;
 mod data2;
@@ -23,5 +24,36 @@ fn main() {
         anything: paramsku,
     };
 
-    println!("{:?}", user)
+    println!("{:?}", user);
+
+    let json_sample = json!(
+        {
+            "name" : "albert",
+            "year_born" : 1995,
+            "phones" : [
+                "2190291",
+                "182918"
+            ]
+        }
+    );
+
+    let json_to_string: Person = serde_json::from_value(json_sample).unwrap();
+
+    println!("{:?}", json_to_string.year_born);
+
+    let string_to_json = to_value(json_to_string).unwrap();
+
+    println!("{:?}", string_to_json);
+
+    let name = "dustin".to_string();
+    let year_born = 1980;
+    let phones = vec![
+        "29189281".to_string(),
+        "38928932".to_string()
+    ];
+
+    let new_object = Person{name, year_born, phones};
+
+    println!("{:?}", new_object);
+
 }
